9894546a9a9cc6c94e046745f3215c33b2d753d2
Christian Zafalon <chris.zafa.2013@gmail.com>
2022-07-01 12:17:11 -0300

    Todo conteudo do primeiro bim + curso da DIO

---
+++ src/app/flexwap-component/flexwap-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flexwap-component',
+  templateUrl: './flexwap-component.component.html',
+  styleUrls: ['./flexwap-component.component.css']
+})
+export class FlexwapComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
